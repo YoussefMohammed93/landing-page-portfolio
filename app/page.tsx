cf98a96d94a762bf4c1b26912995dba1b050d4bf
YoussefMohammed93 <youssefmohammed1590@gmail.com>
2025-05-18 16:23:50 +0300

    Shadcn UI Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,17 @@
 "use client";
 
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 export default function MainPage() {
-  const tasks = useQuery(api.tasks.get);
-
   return (
-    <main className="w-full h-screen flex items-center justify-center">
-      <ul className="flex items-center flex-col gap-5">
-        {tasks?.map((task) => {
-          return (
-            <li
-              key={task._id}
-              className={`text-5xl ${
-                task.isCompleted ? "text-green-500" : "text-yellow-500"
-              }`}
-            >
-              {task.text}
-            </li>
-          );
-        })}
-      </ul>
+    <main>
+      <Button
+        onClick={() => {
+          alert("Hello!");
+        }}
+      >
+        Click
+      </Button>
     </main>
   );
 }
